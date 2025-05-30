41f4ab13f8e375fbad749d431500f48ae92f97c5
SmongsDev <sssm0928@naver.com>
2025-05-31 00:55:56 +0900

    Update setting files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Opcia_V1/', // 저장소 이름으로 변경
   plugins: [react()],
-})
+});
